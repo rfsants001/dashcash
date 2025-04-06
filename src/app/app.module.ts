eab9db9543bf8721bf3741a6204793913862fae8
Rafael Santos <rf.sants001@gmail.com>
2025-04-06 20:40:11 -0300

    chore: upgrade Angular dependencies to version 18.x and refactor app.module.ts for new HttpClient usage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,45 +4,39 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { AuthInterceptor } from './core/services/interceptors/auth.interceptor';
 import { Banknote, BriefcaseBusiness, ChevronDown, ChevronLeft, ChevronRight, Clapperboard, CreditCard, Funnel, KeyRound, LucideAngularModule, Mail, User, Utensils, Wallet } from 'lucide-angular';
 import { AuthComponent } from './pages/auth/auth.component';
 import { AuthFormComponent } from './pages/auth/auth-form/auth-form.component';
 import { SplashComponent } from './pages/splash/splash.component';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    AuthComponent,
-    AuthFormComponent,
-    SplashComponent,
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    LucideAngularModule.pick({
-      CreditCard,
-      User,
-      Mail,
-      KeyRound,
-      Banknote,
-      ChevronDown,
-      ChevronLeft,
-      ChevronRight,
-      Funnel,
-      Utensils,
-    Wallet,
-    Clapperboard,
-  BriefcaseBusiness}),
-    BrowserAnimationsModule
-  ],
-  providers: [{
-    provide: HTTP_INTERCEPTORS,
-    useClass: AuthInterceptor,
-    multi: true
-  }],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent,
+        AuthComponent,
+        AuthFormComponent,
+        SplashComponent,
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        AppRoutingModule,
+        LucideAngularModule.pick({
+            CreditCard,
+            User,
+            Mail,
+            KeyRound,
+            Banknote,
+            ChevronDown,
+            ChevronLeft,
+            ChevronRight,
+            Funnel,
+            Utensils,
+            Wallet,
+            Clapperboard,
+            BriefcaseBusiness
+        }),
+        BrowserAnimationsModule], providers: [{
+            provide: HTTP_INTERCEPTORS,
+            useClass: AuthInterceptor,
+            multi: true
+        }, provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule { }
